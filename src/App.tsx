5e5de1f19b5594318882089ee419255ce4a9dbde
Oskar <case112@users.noreply.github.com>
2023-06-02 10:04:17 +0300

    Button excercise and union type

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
+import Alert from "./components/Alert";
+import { Button } from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  const items = ["New York", "San Fransisco", "Tokyo", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <span>Hello World</span>
+      </Alert>
+      <Button type="danger" onClick={() => console.log("Clicked")}>
+        Button name
+      </Button>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+  children: string;
+  // union type
+  type?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+export const Button = ({ children, type = "primary", onClick }: Props) => {
+  return (
+    <button type="button" className={"btn btn-" + type} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
